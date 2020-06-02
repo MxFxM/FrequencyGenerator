63e96ce6fc51e8aeffb4fced4644a4b483928cc9
MxFxM <mxfxmmueller@gmail.com>
2020-06-02 20:01:17 +0200

    added first version of code

--- function_blocks/GenericJoystick/src/main.cpp
+++ function_blocks/GenericJoystick/src/main.cpp
@@ -1,9 +1,34 @@
 #include <Arduino.h>
 
+#define JoystickX A0
+#define JoystickY A1
+#define JoystickSW 0
+
 void setup() {
   // put your setup code here, to run once:
+
+  pinMode(JoystickX, INPUT);
+  pinMode(JoystickY, INPUT);
+  pinMode(JoystickSW, INPUT_PULLUP);
+
+  Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+
+  int raw_joystick_x = analogRead(JoystickX);
+  int raw_joystick_y = analogRead(JoystickY);
+  boolean val_joystick_sw = digitalReadFast(JoystickSW);
+
+  int val_joystick_x = map(raw_joystick_x, 0, 4095, -5, 5);
+  int val_joystick_y = map(raw_joystick_y, 0, 4095, -5, 5);
+
+  Serial.print("X=");
+  Serial.print(val_joystick_x);
+  Serial.print("   Y=");
+  Serial.print(val_joystick_y);
+  Serial.print("   SW=");
+  Serial.print(val_joystick_sw);
+  Serial.println();
 }
