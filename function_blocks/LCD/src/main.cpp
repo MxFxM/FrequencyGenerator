5fedf0c49dcc963cf4f6fd980e2de054e9a59bda
MxFxM <mxfxmmueller@gmail.com>
2020-06-03 22:12:39 +0200

    testing decimal points for mhz and khz

--- function_blocks/LCD/src/main.cpp
+++ function_blocks/LCD/src/main.cpp
@@ -20,5 +20,98 @@ void loop() {
   // (note: line 1 is the second row, since counting begins with 0):
   lcd.setCursor(0, 1);
   // print the number of seconds since reset:
-  lcd.print(millis() / 1000);
+//  lcd.print(millis() / 1000);
+
+  int freq = micros();
+  int freq_temp = freq;
+
+  int millions = 0;
+  int thousands = 0;
+  int ones = 0;
+
+  if (freq_temp >= 1000000) {
+    millions = floor(freq_temp / 1000000);
+  }
+
+  freq_temp = freq_temp - millions;
+
+  if (freq_temp >= 1000) {
+    thousands = floor(freq / 1000);
+  }
+
+  freq_temp = freq_temp - thousands;
+
+  ones = freq_temp;
+
+
+  if (millions != 0) {
+    // leading blank spaces
+    if (millions >= 10) { // no more than 20M anyway
+      lcd.print("   ");
+    } else {
+      lcd.print("    ");
+    }
+    // millions
+    lcd.print(millions);
+    // millions decimal point
+    lcd.print(".");
+  } else {
+    // leading blank spaces, empty millions and no decimal point
+    lcd.print("      ");
+  }
+
+  if (thousands != 0) {
+    // leading blank spaces, if millions is 0, otherwise leading zeroes
+    if (millions == 0) {
+      if (thousands < 100) {
+        lcd.print(" ");
+      }
+      if (thousands < 10) {
+        lcd.print(" ");
+      }
+    } else {
+      if (thousands < 100) {
+        lcd.print("0");
+      }
+      if (thousands < 10) {
+        lcd.print("0");
+      }
+    }
+    // thousands
+    lcd.print(thousands);
+    // thousands decimal point
+    lcd.print(".");
+  } else {
+    if (millions != 0) {
+      lcd.print("000.");
+    } else {
+      lcd.print("    ");
+    }
+  }
+
+  if (ones != 0) {
+    // leading blank spaces, if millions and thousands are 0, otherwise leading zeroes
+    if (millions == 0 && thousands == 0) {
+      if (ones < 100) {
+        lcd.print(" ");
+      }
+      if (ones < 10) {
+        lcd.print(" ");
+      }
+    } else {
+      if (ones < 100) {
+        lcd.print("0");
+      }
+      if (ones < 10) {
+        lcd.print("0");
+      }
+    }
+    // ones
+    lcd.print(ones);
+  } // there is no else, since this cant be 0
+
+//  lcd.print(freq);
+  lcd.print(" Hz");
+
+  delay(500);
 }
