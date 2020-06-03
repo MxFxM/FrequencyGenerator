38272ea204e516aef6ef9093c48525ca121f1e9d
MxFxM <mxfxmmueller@gmail.com>
2020-06-03 22:22:14 +0200

    added headline to function

--- function_blocks/LCD/src/main.cpp
+++ function_blocks/LCD/src/main.cpp
@@ -11,10 +11,6 @@ LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 void printFrequency(int freq);
 
 void setup() {
-  // set up the LCD's number of columns and rows:
-  lcd.begin(16, 2);
-  // Print a message to the LCD.
-  lcd.print("Frequency:");
 }
 
 void loop() {
@@ -29,6 +25,11 @@ void loop() {
 }
 
 void printFrequency(int freq) {
+  // set up the LCD's number of columns and rows:
+  lcd.begin(16, 2);
+  // Print a message to the LCD.
+  lcd.print("Frequency:");
+
   // set cursor on second line
   lcd.setCursor(0, 1);
 
