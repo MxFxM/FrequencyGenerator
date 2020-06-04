db3d6084f304dd40b7ccb8616651b8b2ab9dfa39
MxFxM <mxfxmmueller@gmail.com>
2020-06-04 12:36:50 +0200

    added adafruit gps library

--- function_blocks/GPSNeo6m/src/main.cpp
+++ function_blocks/GPSNeo6m/src/main.cpp
@@ -1,9 +1,26 @@
 #include <Arduino.h>
 
+#define GPSSerial Serial2
+
 void setup() {
-  // put your setup code here, to run once:
+  // wait for hardware serial to appear
+  while (!Serial);
+
+  // make this baud rate fast enough to we aren't waiting on it
+  Serial.begin(115200); // its always 12 Mbit/s
+
+  // 9600 baud is the default rate for the Ultimate GPS
+  GPSSerial.begin(9600);
 }
 
+
 void loop() {
-  // put your main code here, to run repeatedly:
+  if (Serial.available()) {
+    char c = Serial.read();
+    GPSSerial.write(c);
+  }
+  if (GPSSerial.available()) {
+    char c = GPSSerial.read();
+    Serial.write(c);
+  }
 }
