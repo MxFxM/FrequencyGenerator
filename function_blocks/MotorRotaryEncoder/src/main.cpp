bcb6d72adc5c04495d3c327cebb676cf3cc173b6
MxFxM <32713448+MxFxM@users.noreply.github.com>
2020-06-05 23:14:59 +0200

    new method using 2 interrupts

--- function_blocks/MotorRotaryEncoder/src/main.cpp
+++ function_blocks/MotorRotaryEncoder/src/main.cpp
@@ -1,93 +1,132 @@
 #include <Arduino.h>
 
-#define pin1 0
-#define pin2 1
+#define ROTARY_PIN_1 0
+#define ROTARY_PIN_2 1
 
-#define pinBuzzer 2
+#define BUZZER_PIN 2
 
-void pinInterrupt(void);
+volatile boolean b_pin1_first = false;
+volatile boolean b_pin2_first = false;
+volatile boolean consecutive = false;
+volatile boolean updateDisplay = false;
+volatile long last_change_time = 0;
+volatile int last_change = 0;
+volatile int count = 0;
+volatile int integrator = 0;
 
-void setup() {
+boolean buzzerState = false;
+
+void ISR_RotaryPin1(void);
+void ISR_RotaryPin2(void);
+void CounterUpdate(int change);
+
+void setup()
+{
   // put your setup code here, to run once:
   Serial.begin(9600);
 
-  pinMode(pin1, INPUT);
-  pinMode(pin2, INPUT);
+  pinMode(ROTARY_PIN_1, INPUT);
+  pinMode(ROTARY_PIN_2, INPUT);
 
-  pinMode(pinBuzzer, OUTPUT);
+  pinMode(BUZZER_PIN, OUTPUT);
 
-  attachInterrupt(pin1, pinInterrupt, CHANGE);
+  attachInterrupt(ROTARY_PIN_1, ISR_RotaryPin1, RISING);
+  attachInterrupt(ROTARY_PIN_2, ISR_RotaryPin2, RISING);
 }
 
-volatile int change = 0;
-int count = 0;
-boolean consecutive = false;
-int integrator = 0;
-long last_change_time = 0;
-int last_change = 0;
-boolean buzzerState = false;
-
-void loop() {
+void loop()
+{
   // put your main code here, to run repeatedly:
   long time = micros(); // once to keep consistent timing over the loop
 
-  if (change != 0) {
-    // reset acceleration if direction was reversed
-    if (change != last_change) {
-      integrator = 0;
-      last_change = change;
-    }
-
-    // increase integrator
-    integrator = integrator+1;
-
-    // make integrator exponential
-    int expo = floor(integrator/10.0);
-
-    // limit exponent
-    if (expo >= 5) { // not faster than 100k per count. thats fast enough for a value 0 < x < 20M
-      expo = 5;
-    }
-
-    // increase count
-    count = count+change*pow(10, expo);
-
-    // limit count
-    if (count >= 20000000) {
-      count = 20000000;
-    } else if (count <= 0) {
-      count = 0;
-    }
-
-    // reset change
-    change = 0;
-
-    // save time for reset of integrator
-    last_change_time = time + 200000; // one fith second to reset exponential acceleration
-
+  if (updateDisplay)
+  {
     // toggle the buzzer
     buzzerState = !buzzerState;
-    digitalWriteFast(pinBuzzer, buzzerState);
+    digitalWriteFast(BUZZER_PIN, buzzerState);
+
+    // print value
+    Serial.println(count);
+
+    updateDisplay = false;
   }
 
+  delay(100);
+}
+
+void ISR_RotaryPin1(void)
+{
+  if (!b_pin2_first)
+  {
+    b_pin1_first = true;
+  }
+  else
+  {
+    CounterUpdate(+1);
+    b_pin2_first = false;
+  }
+}
+
+void ISR_RotaryPin2(void)
+{
+  if (!b_pin1_first)
+  {
+    b_pin2_first = true;
+  }
+  else
+  {
+    CounterUpdate(-1);
+    b_pin1_first = false;
+  }
+}
+
+void CounterUpdate(int change)
+{
+  // reset acceleration if direction was reversed
+  if (change != last_change)
+  {
+    integrator = 0;
+    last_change = change;
+  }
+
+  // increase integrator
+  integrator = integrator + 1;
+
+  // make integrator exponential
+  int expo = floor(integrator / 10.0);
+
+  // limit exponent
+  if (expo >= 5)
+  { // not faster than 100k per count. thats fast enough for a value 0 < x < 20M
+    expo = 5;
+  }
+
+  // increase count
+  count = count + change * pow(10, expo);
+
+  // limit count
+  if (count >= 20000000)
+  {
+    count = 20000000;
+  }
+  else if (count <= 0)
+  {
+    count = 0;
+  }
+
+  // reset change
+  change = 0;
+
+  long time = micros();
+
+  // save time for reset of integrator
+  last_change_time = time + 200000; // one fith second to reset exponential acceleration
+
   // reset integrator if time is over
-  if (time > last_change_time) {
+  if (time > last_change_time)
+  {
     integrator = 0;
   }
 
-  // print value
-  Serial.println(count);
-
-  delay(10);
-}
-
-void pinInterrupt(void) {
-  boolean a = digitalReadFast(pin1);
-  boolean b = digitalReadFast(pin2);
-
-  if (a == b) {
-    change = -1;
-  } else {
-    change = +1;
-  }
+  updateDisplay = true;
 }
