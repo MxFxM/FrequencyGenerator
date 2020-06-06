17d43e1154e0bb61769284f273fbac0d40e63390
MxFxM <mxfxmmueller@gmail.com>
2020-06-07 00:56:59 +0200

    done for today

--- function_blocks/RotaryEncoder/src/main.cpp
+++ function_blocks/RotaryEncoder/src/main.cpp
@@ -1,23 +1,31 @@
 #include <Arduino.h>
 
+// these are the encoder inputs
 #define ENC_A 1
 #define ENC_B 2
 #define ENC_Btn 0
+
+// if the autoset pin is high, the frequency will be written directly when a change occures
+// otherwise the frequency has to be confirmed with the rotary button
+#define PinAutoset 3
 
 #define MAX_COUNTER_VALUE 10000000
 #define MIN_COUNTER_VALUE 1
 
 void read_encoder(void);
 void confirm_frequency(void);
+void setFrequency(int frequency);
 
 volatile int counter = 1;
 volatile int previousCounter = 0;
+volatile boolean autoset = 0;
 
 void setup()
 {
   pinMode(ENC_A, INPUT);
   pinMode(ENC_B, INPUT);
   pinMode(ENC_Btn, INPUT_PULLUP);
+  pinMode(PinAutoset, INPUT_PULLDOWN);
 
   Serial.begin(9600);
 
@@ -28,6 +36,8 @@ void setup()
 
 void loop()
 {
+  autoset = digitalReadFast(PinAutoset);
+
   if (counter != previousCounter) {
     previousCounter = counter;
     Serial.println(counter);
@@ -50,11 +60,17 @@ void read_encoder()
       if (counter >= MAX_COUNTER_VALUE) {
         counter = MAX_COUNTER_VALUE;
       }
+      if (autoset) {
+        setFrequency(counter);
+      }
       tick = false;
     } else if (newState == 3 && previousState == 1 && tick) {
       counter = counter - 1;
       if (counter <= MIN_COUNTER_VALUE) {
         counter = MIN_COUNTER_VALUE;
+      }
+      if (autoset) {
+        setFrequency(counter);
       }
       tick = false;
     } else if (newState == 2 && previousState == 0 && !tick) {
@@ -74,8 +90,13 @@ void confirm_frequency() {
   long time = micros();
 
   if (time > lastConfirm + 100000) {
+    setFrequency(counter);
     Serial.print("Confirmed: ");
     Serial.println(counter);
     lastConfirm = time;
   }
+}
+
+void setFrequency(int frequency) {
+  Serial.println("set");
 }
