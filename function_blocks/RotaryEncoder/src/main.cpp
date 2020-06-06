0369396c0c611e38391c407c2b01ccb5527c9fa7
MxFxM <mxfxmmueller@gmail.com>
2020-06-06 21:53:10 +0200

    added button support

--- function_blocks/RotaryEncoder/src/main.cpp
+++ function_blocks/RotaryEncoder/src/main.cpp
@@ -2,37 +2,42 @@
 
 #define ENC_A 1
 #define ENC_B 2
+#define ENC_Btn 0
+
+#define MAX_COUNTER_VALUE 10000000
+#define MIN_COUNTER_VALUE 1
 
 void read_encoder(void);
+void confirm_frequency(void);
 
-volatile int counter;
-volatile boolean tick;
+volatile int counter = 1;
+volatile int previousCounter = 0;
 
 void setup()
 {
   pinMode(ENC_A, INPUT);
   pinMode(ENC_B, INPUT);
+  pinMode(ENC_Btn, INPUT_PULLUP);
 
   Serial.begin(9600);
-  Serial.println("Start");
-
-  counter = 0;
-  tick = false;
 
   attachInterrupt(ENC_A, read_encoder, CHANGE);
   attachInterrupt(ENC_B, read_encoder, CHANGE);
+  attachInterrupt(ENC_Btn, confirm_frequency, FALLING);
 }
 
 void loop()
 {
-  // do some stuff here - the joy of interrupts is that they take care of themselves
-  Serial.println(counter);
-  delay(100);
+  if (counter != previousCounter) {
+    previousCounter = counter;
+    Serial.println(counter);
+  }
 }
 
 void read_encoder()
 {
   static uint8_t previousState = 3;
+  static boolean tick = false;
 
   uint8_t newA = digitalReadFast(ENC_A);
   uint8_t newB = digitalReadFast(ENC_B);
@@ -42,26 +47,35 @@ void read_encoder()
   if (newState != previousState) {
     if (newState == 3 && previousState == 2 && tick) {
       counter = counter + 1;
+      if (counter >= MAX_COUNTER_VALUE) {
+        counter = MAX_COUNTER_VALUE;
+      }
       tick = false;
-    } else
-
-    if (newState == 3 && previousState == 1 && tick) {
+    } else if (newState == 3 && previousState == 1 && tick) {
       counter = counter - 1;
+      if (counter <= MIN_COUNTER_VALUE) {
+        counter = MIN_COUNTER_VALUE;
+      }
       tick = false;
-    } else
-
-    if (newState == 2 && previousState == 0 && !tick) {
+    } else if (newState == 2 && previousState == 0 && !tick) {
       tick = true;
-    } else
-
-    if (newState == 1 && previousState == 0 && !tick) {
+    } else if (newState == 1 && previousState == 0 && !tick) {
       tick = true;
-    } else
-
-    {
+    } else {
       tick = false;
     }
 
     previousState = newState;
+  }
+}
+
+void confirm_frequency() {
+  static long lastConfirm = 0;
+  long time = micros();
+
+  if (time > lastConfirm + 100000) {
+    Serial.print("Confirmed: ");
+    Serial.println(counter);
+    lastConfirm = time;
   }
 }
