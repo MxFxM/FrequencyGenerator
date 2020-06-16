53fda9e37f883da9391322df1910afe4dc29280b
MxFxM <32713448+MxFxM@users.noreply.github.com>
2020-06-16 16:48:49 +0200

    implementing gps module

--- software/FrequencyGenerator/src/main.cpp
+++ software/FrequencyGenerator/src/main.cpp
@@ -76,6 +76,21 @@ volatile boolean rot_short_press = false;
 #define LED_BLUE 14
 
 // ------------------------------------------------------------------------------------------------
+// GPS module
+// ------------------------------------------------------------------------------------------------
+#define GPS Serial5
+
+// some predefined messages (wrong crc)
+uint8_t message_coldstart[] = {0xB5, 0x62, 0x06, 0x04, 0x04, 0x00, 0xFF, 0xFF, 0x02, 0x00, 0x0E, 0x61};
+uint8_t message_warmstart[] = {0xB5, 0x62, 0x06, 0x04, 0x04, 0x00, 0xFF, 0xFF, 0x02, 0x00, 0x0E, 0x61};
+uint8_t message_hotstart[]  = {0xB5, 0x62, 0x06, 0x04, 0x04, 0x00, 0xFF, 0xFF, 0x02, 0x00, 0x0E, 0x61};
+
+// functions
+void calcCRC(uint8_t* message);
+void configureTimepulse(uint32_t freq, uint32_t freqLock, double pulseRatio, double pulseRatioLock);
+void setupGPS(void);
+
+// ------------------------------------------------------------------------------------------------
 // State machine and menus
 // ------------------------------------------------------------------------------------------------
 enum main_states
@@ -402,6 +417,12 @@ void setupRotaryEncoder(void) {
   attachInterrupt(ROT_SW, pressEncoder, CHANGE);
 }
 
+void setupGPS(void) {
+  GPS.begin(9600);
+  delay(500);
+  configureTimepulse(1, 1, 0, 0);
+}
+
 void printFrequency(int freq) {
   // print the top row
   lcd.setCursor(0, 0);
@@ -669,8 +690,85 @@ void pressEncoder(void) {
   }
 }
 
-void setFrequency(int frequency) {
-  #ifdef DEBUG_MODE
-  Serial.println("set");
-  #endif
+void calcCRC(uint8_t* message) {
+  uint8_t ck_a = 0;
+  uint8_t ck_b = 0;
+
+  uint16_t length = 8;
+
+  // advance 2 adresses to skip header
+  message++;
+  message++;
+
+  for (uint16_t i = 2; i < length - 2; i++) {
+    if (i == 4) {
+      length = length + *message;
+    }
+    if (i == 5) {
+      length = length + (*message << 8);
+    }
+    // calculate the checksum
+    ck_a = ck_a + *message;
+    ck_b = ck_b + ck_a;
+
+    message++; // advance pointer to next byte
+  }
+
+  // add crc to message
+  *message = ck_a;
+  message++;
+  *message = ck_b;
+}
+
+void configureTimepulse(uint32_t freq, uint32_t freqLock, double pulseRatio, double pulseRatioLock) {
+  double factor = powl(2.0, -32.0);
+  pulseRatio = 1 - pulseRatio;
+  uint32_t dc = (pulseRatio / factor);
+  pulseRatioLock = 1 - pulseRatioLock;
+  uint32_t dcLock = (pulseRatioLock / factor);
+
+  uint8_t message[40];
+  message[0] =  0xB5; // header
+  message[1] =  0x62; // header
+  message[2] =  0x06; // class
+  message[3] =  0x31; // id
+  message[4] =  0x20; // length
+  message[5] =  0x00; // length
+  message[6] =  0x00; // time pulse selection
+  message[7] =  0x01; // version
+  message[8] =  0x00; // reserved
+  message[9] =  0x00; // reserved
+  message[10] = 0x32; // antenna cable delay (here fixed)
+  message[11] = 0x00; // antenna cable delay
+  message[12] = 0x00; // rf group delay (here fixed)
+  message[13] = 0x00; // rf group delay
+  message[14] = (freq >>  0) & 0xFF; // frequency
+  message[15] = (freq >>  8) & 0xFF; // frequency
+  message[16] = (freq >> 16) & 0xFF; // frequency
+  message[17] = (freq >> 24) & 0xFF; // frequency
+  message[18] = (freqLock >>  0) & 0xFF; // frequency on lock
+  message[19] = (freqLock >>  8) & 0xFF; // frequency on lock
+  message[20] = (freqLock >> 16) & 0xFF; // frequency on lock
+  message[21] = (freqLock >> 24) & 0xFF; // frequency on lock
+  message[22] = (dc >>  0) & 0xFF; // dutycycle
+  message[23] = (dc >>  8) & 0xFF; // dutycycle
+  message[24] = (dc >> 16) & 0xFF; // dutycycle
+  message[25] = (dc >> 24) & 0xFF; // dutycycle
+  message[26] = (dcLock >>  0) & 0xFF; // dutycycle on lock
+  message[27] = (dcLock >>  8) & 0xFF; // dutycycle on lock
+  message[28] = (dcLock >> 16) & 0xFF; // dutycycle on lock
+  message[29] = (dcLock >> 24) & 0xFF; // dutycycle on lock
+  message[30] = 0x00; // user configured delay
+  message[31] = 0x00; // user configured delay
+  message[32] = 0x00; // user configured delay
+  message[33] = 0x00; // user configured delay
+  message[34] = 0xEF; // flags
+  message[35] = 0x00; // flags
+  message[36] = 0x00; // flags
+  message[37] = 0x00; // flags
+  message[38] = 0x00; // crc will be included after calculation
+  message[39] = 0x00; // crc will be included after calculation
+
+  calcCRC(&message[0]);
+  GPS.write(message, sizeof(message));
 }
